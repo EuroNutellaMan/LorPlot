add6d3e5da0bfec9c822e8afb0b5853cb9c88da4
EuroNutellaMan <lorenzo.brzek@proton.me>
2024-05-05 02:04:02 +0200

    Ignore Examples folder and fix plot.csv
    - Cloning with github now shouldn't download the Examples folder
    - The user can now choose to review that everything is fine in the
    plot.csv file
    - If no plot.csv is found the program will create it

--- LorPlot.py
+++ LorPlot.py
@@ -26,6 +26,47 @@ install_path = os.path.abspath(__file__)
 install_dir = os.path.dirname(install_path)
 lineplotter = install_dir + '/lineplotter.py'
 barplotter = install_dir + '/barplotter.py'
+csvfile = install_dir + '/plot.csv'
+
+# Check that plot.csv exists
+if not os.path.exists(csvfile):
+    print(f"plot.csv not found, creating file now!")
+    try:
+        with open(csvfile, 'w'):
+            pass
+        print(f"CSV file created successfully at: {csvfile}")
+    except Exception as e:
+        print(f"Failed to create the CSV file: {e}")
+    print(f"Please write values in {csvfile} before using the program!")
+    print(f"Make sure to insert 3 values per row!")
+    exit
+
+# Print out values in plot.csv
+def reviewer():
+    missing_value = 0
+    print(f"Reading {csvfile}")
+    with open(csvfile, "r") as file:
+        lines = file.readlines()
+    os.system('clear')
+    for line in lines:
+        check_values = line.split(',')
+        if len(check_values) != 3:
+            print(f"{RED}{line} INVALID AMOUNT OF VALUES! Must be 3!")
+            missing_value = 1
+        else:
+            try:
+                float(check_values[2])
+                float(check_values[1])
+                print(f"{GREEN}{line}")
+            except:
+                print(f"{RED}{line} INVALID VALUES! 2nd and 3rd column must be numbers!")
+                missing_value = 1
+    if bool(missing_value):
+        print(f"{RED}One or more rows are invalid, please edit the csv file to fix this!")
+    else:
+        print(f"{GREEN}Everything seems in order!")
+        proceed = input("Press enter to go back to selection: ")
+        selector()
 
 # Function to choose type of graph
 def selector():
@@ -33,6 +74,7 @@ def selector():
     print(YELLOW + 'Select graph style')
     print(GREEN + '1: Line plot')
     print(BLUE + '2: Bar plot')
+    print(YELLOW + '9: Review plot.csv')
     print(RED + '0: Exit')
     choice = input('Select option [1-2, 0]: ')
     if choice == '0': # Exits the program
@@ -41,6 +83,8 @@ def selector():
         subprocess.Popen(['python3', lineplotter]).wait()
     elif choice == '2': # Starts the bar plotter
         subprocess.Popen(['python3', barplotter]).wait()
+    elif choice == '9':
+        reviewer()
     else: # Restarts in case of bad prompt from user
         print(RED + 'Invalid input! Try again!')
         print(RED + 'Restarting in 3...')
